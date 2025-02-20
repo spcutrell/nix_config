50076c33fee051233f518a19ff5c431341db6b91
Spencer Cutrell <lv426@USCSSNostromo.verizon.net>
2025-02-19 22:55:07 -0500

    Ad more robust fish config

--- home/users/dallas/features/fish.nix
+++ home/users/dallas/features/fish.nix
@@ -1,16 +1,22 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.fish = {
     enable = true;
-    # programs.fish.plugins = [
-    #   {
-    #     name = "z";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "jethrokuan";
-    #       repo = "z";
-    #       rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
-    #       sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
-    #     };
-    #   }  
-    # ];
+
+    programs.fish.plugins = builtins.attrValues {
+      inherit (pkgs.fishPlugins)
+        z
+        hydro
+      ;
+    };
+
+    shellInit = ''
+      set -U Z_CMD "j"
+    '';
+
+    shellAliases = {
+      tree = "eza --all --long --tree";
+      mv = "mv -i";
+      cp = "cp -ia";
+    };
   };
 }
